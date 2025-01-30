9d9a84a4faf8f678ce742282276b732ddce11120
ChillerDragon <ChillerDragon@gmail.com>
2025-01-30 16:08:21 +0100

    gdm: fix crash when left players make kills
    ``pKiller`` can be a ``nullptr`` if the killer leaves
    the game while his grenade is still in the air.
    
    Also the entire win check code is not needed anymore.
    The base controller does a wincheck on tick which also
    covers dm based modes.

--- src/game/server/gamemodes/instagib/dm.cpp
+++ src/game/server/gamemodes/instagib/dm.cpp
@@ -23,14 +23,5 @@ void CGameControllerInstaBaseDM::OnCharacterSpawn(class CCharacter *pChr)
 
 int CGameControllerInstaBaseDM::OnCharacterDeath(class CCharacter *pVictim, class CPlayer *pKiller, int WeaponId)
 {
-	CGameControllerPvp::OnCharacterDeath(pVictim, pKiller, WeaponId);
-
-	// check score win condition
-	if((m_GameInfo.m_ScoreLimit > 0 && pKiller->m_Score.value_or(0) >= m_GameInfo.m_ScoreLimit) ||
-		(m_GameInfo.m_TimeLimit > 0 && (Server()->Tick() - m_GameStartTick) >= m_GameInfo.m_TimeLimit * Server()->TickSpeed() * 60))
-	{
-		EndRound();
-		return true;
-	}
-	return false;
+	return CGameControllerPvp::OnCharacterDeath(pVictim, pKiller, WeaponId);
 }
