8a3296dea8491cd13f667a83b8457e40baaae1b9
ChillerDragon <chillerdragon@gmail.com>
2026-04-30 17:08:55 +0200

    merge: fix build after merge (missing isdigit() include)

--- src/insta/server/strhelpers.cpp
+++ src/insta/server/strhelpers.cpp
@@ -3,6 +3,8 @@
 #include <base/str.h>
 #include <base/time.h>
 
+#include <cctype>
+
 const char *str_find_digit(const char *Haystack)
 {
 	char aaDigits[][2] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
