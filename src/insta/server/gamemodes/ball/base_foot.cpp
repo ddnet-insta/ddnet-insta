ef1b4ec72fc4aade4a07976ab2dc47cbffd44b48
ChillerDragon <chillerdragon@gmail.com>
2026-05-01 09:30:52 +0200

    refactor: remove unnecessary projectile code for ball modes
    CC https://github.com/ddnet-insta/ddnet-insta/pull/647#issuecomment-4353875922

--- src/insta/server/gamemodes/ball/base_foot.cpp
+++ src/insta/server/gamemodes/ball/base_foot.cpp
@@ -334,7 +334,7 @@ void CGameControllerBaseFoot::FireGrenade(CCharacter *Character, vec2 Direction,
 		return;
 	}
 
-	CFootProjectile *pProj = new CFootProjectile(
+	new CFootProjectile(
 		&GameServer()->m_World,
 		WEAPON_GRENADE,
 		pPlayer->GetCid(),
@@ -345,10 +345,6 @@ void CGameControllerBaseFoot::FireGrenade(CCharacter *Character, vec2 Direction,
 		false,
 		SOUND_GRENADE_EXPLODE,
 		MouseTarget);
-
-	// pack the Projectile and send it to the client Directly
-	CNetObj_Projectile p;
-	pProj->FillInfo(&p);
 
 	GameServer()->CreateSound(Character->GetPos(), SOUND_GRENADE_FIRE, Character->TeamMask());
 	Character->LoseBall();
