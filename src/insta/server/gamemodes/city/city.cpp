a65f5aa65667f8c21925e0988139098834aac4b3
ChillerDragon <chillerdragon@gmail.com>
2026-05-24 09:35:23 +0200

    good enuff for me

--- src/insta/server/gamemodes/city/city.cpp
+++ src/insta/server/gamemodes/city/city.cpp
@@ -46,6 +46,14 @@ void CGameControllerCity::OnCharacterSpawn(CCharacter *pChr)
 
 int CGameControllerCity::OnCharacterDeath(CCharacter *pVictim, class CPlayer *pKiller, int Weapon)
 {
+	if(pKiller)
+	{
+		int Killer = pKiller->GetCid();
+		pKiller->m_Account.m_Mode.m_Mmo.m_Stones++;
+		char aBuf[512];
+		str_format(aBuf, sizeof(aBuf), "+1 stone you now have %d stones", pKiller->m_Account.m_Mode.m_Mmo.m_Stones);
+		SendChatTarget(Killer, aBuf);
+	}
 	return 0;
 }
 
